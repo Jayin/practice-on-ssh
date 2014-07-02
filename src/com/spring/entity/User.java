6492a9fa7d75bc501c28033efcb450ce77320579
Jayin Ton <tonjayin@gmail.com>
2014-07-02 11:49:46 +0800

    AOP

--- src/com/spring/entity/User.java
+++ src/com/spring/entity/User.java
@@ -1,5 +1,14 @@
 package com.spring.entity;
 
 public class User {
+	private String userName;
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
 	
 }
